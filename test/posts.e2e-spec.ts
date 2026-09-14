6597a3b3881d92a2f1c4eb48642b1de34e3cf677
EdgarEldy <ed.eldy21@live.com>
2026-09-14 15:45:41 +0400

    fix(common): use the shared createValidationPipe in posts.e2e-spec.ts
    One fewer copy of the inline pipe config this branch's review flagged as
    duplicated four times over; this was one of the four.

--- test/posts.e2e-spec.ts
+++ test/posts.e2e-spec.ts
@@ -1,11 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BadRequestException, INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { randomUUID } from 'crypto';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
 import { HttpExceptionFilter } from '../src/common/filters/http-exception.filter';
+import { createValidationPipe } from '../src/common/pipes/create-validation-pipe';
 
 interface AuthedUser {
   token: string;
@@ -43,17 +44,7 @@ describe('Posts (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        exceptionFactory: (errors) => {
-          const message = errors
-            .map((e) => Object.values(e.constraints ?? {}).join(', '))
-            .join('; ');
-          return new BadRequestException(message);
-        },
-      }),
-    );
+    app.useGlobalPipes(createValidationPipe());
     app.useGlobalFilters(new HttpExceptionFilter());
     await app.init();
 
