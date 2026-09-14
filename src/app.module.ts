4689a23084f627f4c00dae2ed23b699ddb5861b0
EdgarEldy <ed.eldy21@live.com>
2026-09-14 12:15:39 +0400

    feat(comments): register CommentsModule in AppModule
    Mounts GET/POST /posts/:postId/comments and DELETE /comments/:id.

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { HealthController } from './health/health.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -31,6 +32,7 @@ import { PostsModule } from './posts/posts.module';
     AuthModule,
     UsersModule,
     PostsModule,
+    CommentsModule,
   ],
   controllers: [HealthController],
   providers: [{ provide: APP_GUARD, useClass: AuthGuard }],
